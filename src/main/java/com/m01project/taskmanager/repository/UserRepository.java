1173a438ec57b498cc57b84b30ddb1f5dbec56e3
Toyli-04 <agajumayevtoyly@gmail.com>
2025-11-18 14:41:37 +0530

    homework-7 created user entity and lombok

---
+++ src/main/java/com/m01project/taskmanager/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.m01project.taskmanager.repository;
+
+import com.m01project.taskmanager.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
+
